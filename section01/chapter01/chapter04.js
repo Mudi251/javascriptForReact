e26e8d19d235ed42573d383fd76e05d6f5564e54
mudi <freedom2424@nate.com>
2026-02-03 10:41:02 +0900

    [추가] 자바스크립트에서 변수명명 규칙이다.

---
+++ section01/chapter01/chapter04.js
@@ -0,0 +1,19 @@
+
+// 1. 변수 
+let no = 27; 
+let age; 
+age = 30; 
+//let age = 40;  //(x)  
+console.log(age); 
+ 
+// 2. 상수  
+const birth = "1997.01.07"; 
+//const birth2;    //상수는 반드시 초기화를 해야 된다. 
+//birth=”123”;   //(x) 
+  
+// 3. 변수 명명규칙(네이밍 규칙) 
+// 3-1. $, _ 제외한 특수기호는 사용할 수 없다. 
+let $_name; 
+//let ^age = 25;  console.log(^age);  //에러가 발생함 
+//let +age = 25;  console.log(+age);  //에러가 발생함 
+let _age = 30;  console.log(_age);
